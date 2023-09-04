2f70bfc81345663889e282abf68df1e6ceb01da7
ThailandBoy <91521829+ThailandBoy@users.noreply.github.com>
2023-09-04 22:05:30 +0300

    Убрал не исп. модели и поставил String.

--- mypp3_1_5/src/main/java/ru/chuldum/mypp3_1_5/controllers/AdminController.java
+++ mypp3_1_5/src/main/java/ru/chuldum/mypp3_1_5/controllers/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -45,22 +44,22 @@ public class AdminController {
     }
 
     @PostMapping("/saveWrapper")
-    public ResponseEntity saveWrapper(@ModelAttribute("userRoleWrap") UserRoleWrap userRoleWrap, Model model) {
+    public ResponseEntity<String> saveWrapper(@ModelAttribute("userRoleWrap") UserRoleWrap userRoleWrap) {
         try {
             userService.saveWrapper(userRoleWrap);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e);
+            return ResponseEntity.badRequest().body("Ошибка при сохранении пользователя!");
         }
-        return ResponseEntity.ok(HttpStatus.OK);
+        return ResponseEntity.ok().body(HttpStatus.OK.toString());
     }
 
     @PostMapping("/deleteUser")
-    public ResponseEntity deleteThroughId(@ModelAttribute("userRoleWrap") UserRoleWrap userRoleWrap, Model model) {
+    public ResponseEntity<String> deleteThroughId(@ModelAttribute("userRoleWrap") UserRoleWrap userRoleWrap) {
         try {
             userService.deleteViaId(userRoleWrap.getUser().getId());
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e);
+            return ResponseEntity.badRequest().body("Ошибка при удалении пользователя!");
         }
-        return ResponseEntity.ok(HttpStatus.OK);
+        return ResponseEntity.ok().body(HttpStatus.OK.toString());
     }
 }
